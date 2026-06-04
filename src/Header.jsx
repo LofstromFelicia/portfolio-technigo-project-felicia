3a448adab7a1e780f60d2a00167c08bd5d0bea0a
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-04 15:34:45 +0200

    style: fully customized maker-board with cinyl stickers and polaroid frames

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,33 +1,30 @@
 export const Header = () => {
   return (
-    <header className="hero-section pinned-block">
+    <>
+      <header className="hero-section pinned-block">
+        <span className="block-pin pin-top-center">📌</span>
 
-      <span className="block-pin pin-top center">📌</span>
-
-      <div className="hero-text-content">
-        <p className="hero-greeting">Hej, My name is Felicia Löfström <span className="animated-emoji">🌻</span></p>
-
-        <h1>Aspiring Web Developer</h1>
-
-        <div className="hero-avatar-container">
-          <img src="/profile-pic.jpg" alt="Felicia Löfström" className="hero-avatar" />
+        <div className="hero-text-content">
+          <p className="hero-greeting">Hej, My name is Felicia Löfström <span className="animated-emoji">🌻</span></p>
+          <h1>Aspiring Web Developer</h1>
+          <div className="hero-avatar-container">
+            <img src="/profile-pic.jpg" alt="Felicia Löfström" className="hero-avatar" />
+          </div>
+          <p className="hero-description">
+            Welcome to my portfolio!
+            Here is a selection of what I have created during my web developing bootcamp!
+          </p>
         </div>
+      </header>
 
-        <p className="hero-description">
-          Welcome to my portfolio!
-          With an eye for details and passion for structured code, my aim is to build modern web applications.
-          Here is a selection of what I have created during my web developing bootcamp!
-        </p>
-
-        <div className="hero-socials">
-          <a href="https://github.com/LofstromFelicia" target="_blank" rel="noopener noreferrer" title="GitHub">
-            <i className="fa-brands fa-github"></i> GitHub
-          </a>
-          <a href="www.linkedin.com/in/felicia-löfström-693390130" target="_blank" rel="noopener noreferrer" title="LinkedIn">
-            <i className="fa-brands fa-linkedin"></i> LinkedIn
-          </a>
-        </div>
+      <div className="hero-socials">
+        <a href="https://github.com/LofstromFelicia" target="_blank" rel="noopener noreferrer" className="sticker-github" title="GitHub">
+          <img src="/github-sticker.png" alt="Github" />
+        </a>
+        <a href="www.linkedin.com/in/felicia-löfström-693390130" target="_blank" rel="noopener noreferrer" className="sticker-linkedin" title="LinkedIn">
+          <img src="/linkedin-sticker.png" alt="LinkedIn" />
+        </a>
       </div>
-    </header>
+    </>
   )
 }
